b549009d8eeab81160bf22bfc44d3889b4376dac
Eirik <enoseber@stud.ntnu.no>
2018-01-10 14:12:42 +0100

    Created test which gets data from Database

--- src/test/java/data/LoginTest.java
+++ src/test/java/data/LoginTest.java
@@ -15,8 +15,8 @@ public class LoginTest {
         // Set up mock
         mockedUserDao = mock(UserDao.class);
 
-        when(mockedUserDao.getUser("bk@asd.no"))
-                .thenReturn(new User("bk@asd.no","bk","123","pass123"));
+//        when(mockedUserDao.getUser("bk@asd.no"))
+//                .thenReturn(new User("bk@asd.no","bk","123","pass123"));
     }
 
     @Test
@@ -25,5 +25,17 @@ public class LoginTest {
         assertEquals(new User("bk@asd.no","bk","123","pass123")
                 , mockedUserDao.getUser("bk@asd.no"));
 
+    }
+
+    @Test
+    public void hent_user(){
+        UserDao u = new UserDao();
+        String s;
+        try{
+            s = u.getUser("tre@h.no").toString();
+            System.out.println(s);
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
     }
 }
