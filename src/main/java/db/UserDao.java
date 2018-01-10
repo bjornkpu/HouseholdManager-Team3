299845f0d1f89146ab96f2d4c3078c7c2a6bf310
Johan Martin Skavhaug <johanmsk@stud.ntnu.no>
2018-01-10 09:30:43 +0100

    Logon start 0.1

--- src/main/java/db/UserDao.java
+++ src/main/java/db/UserDao.java
@@ -1,18 +1,75 @@
 package db;
 
 import data.User;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import util.Logger;
 
+/**
+ * @author jmska
+ */
 public class UserDao {
 
+    private static final Logger log = Logger.getLogger();
 
     public User getUser(String email) throws SQLException {
-
-        return null;
+        Connection connection = Db.instance().getConnection();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery("SELECT * FROM user WHERE email='" + email + "'");
+            User user = null;
+            if(rs.next()) {
+                log.info("Found user " + email);
+                user = new User();
+                user.setEmail(rs.getString("email"));
+                user.setFirstName(rs.getString("first_name"));
+                user.setLastName(rs.getString("last_name"));
+                user.setPassword(rs.getString("password"));
+            } else {
+                log.info("Could not find user " + email);
+            }
+            rs.close();
+            statement.close();
+            return user;
+        } finally {
+            connection.close();
+        }
     }
 
-    public void addUser(User user) throws SQLException{
+    public boolean addUser(User user) throws SQLException {
+        Connection connection = Db.instance().getConnection();
+        try {
+            Statement s = connection.createStatement();
+            int result = s.executeUpdate(
+                    "INSERT INTO user (email,first_name,last_name,password) VALUES('" +
+                            user.getEmail() + "','" +
+                            user.getFirstName() + "','" +
+                            user.getLastName()+ "','" +
+                            user.getPassword() + "')");
+            s.close();
+            log.info("Add user " + (result == 1?"ok":"failed"));
+            return result == 1;
+        } finally {
+            connection.close();
+        }
+    }
 
+    public boolean updateUser(User user) throws SQLException {
+        Connection connection = Db.instance().getConnection();
+        try {
+            Statement s = connection.createStatement();
+            int result = s.executeUpdate(
+                    "UPDATE user set first_name='" + user.getFirstName() +
+                            "',last_name='" + user.getLastName() +
+                            "',password='" + user.getPassword() +
+                            "' where email='" + user.getEmail() + "'");
+            s.close();
+            log.info("Update user " + (result == 1?"ok":"failed"));
+            return result == 1;
+        } finally {
+            connection.close();
+        }
     }
 }
