20755e352b77ff4d6540004406386a09c4ac4c9a
Martin Wangen <martwa@stud.ntnu.no>
2018-01-11 09:12:50 +0100

    nada

--- src/main/java/data/Group.java
+++ src/main/java/data/Group.java
@@ -1,7 +1,48 @@
 package data;
 
+import java.util.ArrayList;
+
 /**
  * Class for the Group-object
  */
 public class Group {
+    private int id;
+    private String name;
+    private int adminid;
+    private ArrayList<Integer> users = new ArrayList<Integer>();
+
+    public Group() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAdminid() {
+        return adminid;
+    }
+
+    public void setAdminid(int adminid) {
+        this.adminid = adminid;
+    }
+
+    public ArrayList<Integer> getUsers() {
+        return users;
+    }
+
+    public void setUsers(ArrayList<Integer> users) {
+        this.users = users;
+    }
 }
