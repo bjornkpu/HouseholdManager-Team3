af5874820daf76dc399300f9574fc5487bbb181f
Martin Wangen <martwa@stud.ntnu.no>
2018-01-12 13:29:12 +0100

    Group brachuru

--- src/main/java/services/GroupService.java
+++ src/main/java/services/GroupService.java
@@ -1,11 +1,13 @@
-/*
+
 
 package services;
 import data.Group;
 import data.User;
+import db.GroupDao;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import java.sql.SQLException;
 import java.util.*;
 
 
@@ -16,45 +18,12 @@ import java.util.*;
         @GET
         @Path("/{groupid}")
         @Produces(MediaType.APPLICATION_JSON)
-        public Group getGroup(@PathParam("groupid") int groupid) {
-            if(kunder.get(email)!=null){
-                return kunder.get(email);
+        public Group getGroup(@PathParam("groupid") int groupid) throws SQLException {
+            Group result = GroupDao.getGroup(groupid);
+            if(result!=null){
+                return result;
             }else {
                 throw new javax.ws.rs.NotFoundException();
             }
-
-        }
-        @GET
-        @Produces(MediaType.APPLICATION_JSON)
-        public List<User> getUserlist(int groupId) {
-            return kunder.values();
-        }
-
-        @POST
-        @Consumes(MediaType.APPLICATION_JSON)
-        public void addGroup(Group kunde) {
-            kunder.put(kunde.getId(), kunde);
-        }
-
-        @DELETE
-        @Path("/{email}")
-        @Produces(MediaType.APPLICATION_JSON)
-        public void deleteGroup(@PathParam("email") String email){
-            kunder.remove(email);
-        }
-
-        @PUT
-        @Path("/{email}")
-        @Produces(MediaType.APPLICATION_JSON)
-        public void updateGroup(Group kunde){
-            if(kunder.get(kunde.getId())!=null){
-                kunder.put(kunde.getId(),kunde);
-            }else {
-                throw new javax.ws.rs.NotFoundException();
-
-            }
-        }
     }
-
 }
-*/
