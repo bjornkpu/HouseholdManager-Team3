f32c76821de90e9cec0eff60d91cbd3e5e3b987e
krisnyb <krisnyb@stud.ntnu.no>
2018-01-15 15:15:56 +0100

    Maybe make the constants public

--- src/main/java/util/UserStatus.java
+++ src/main/java/util/UserStatus.java
@@ -16,32 +16,32 @@ public class UserStatus {
      *
      */
 
-    private final static int PENDING = 0;
+    public final static int PENDING = 0;
 
     /**
      * The user is a member of the group
      *
      */
 
-    private final static int MEMBER = 1;
+    public final static int MEMBER = 1;
 
     /**
      * The user is a member of the group and the admin of the group.
      *
      */
 
-    private final static int ADMIN = 2;
+    public final static int ADMIN = 2;
 
     /**
      * The user account is a member of the group but have had his access disabled.
      */
 
-    private final static int DISABLED = 3;
+    public final static int DISABLED = 3;
 
     /**
      * The user is banned from the group.
      *
      */
 
-    private final static int BANNED = -1;
+    public final static int BANNED = -1;
 }
