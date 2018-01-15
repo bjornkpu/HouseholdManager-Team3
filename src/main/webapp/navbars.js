40bdaca26902c480e4e4f022b53a9cd1103043b8
drHellWool <eirikovs123@hotmail.no>
2018-01-15 13:35:21 +0100

    Content loading into main group page

--- src/main/webapp/navbars.js
+++ src/main/webapp/navbars.js
@@ -9,5 +9,29 @@ $(document).ready(function(){
     $(".dropdown").on("show.bs.dropdown", function(){
         $(".btn").html('My groups <span class="caret caret-up"></span>');
     });
+
+
+
+    if (window.location.hash == "#shopping"){
+        $("#page-content").load("Shoppinglist.html");
+    };
+
+
+    $("#loadShoppingList").click(function(){
+        $("#page-content").load("Shoppinglist.html");
+    });
+
+    $("#loadFeed").click(function(){
+        $("#page-content").load("Feed.html");
+    });
+
+    $("#loadStatistics").click(function(){
+        $("#page-content").load("Statistic.html");
+    });
+
+    $("#loadTasks").click(function(){
+        $("#page-content").load("Tasks.html");
+    });
+
 });
 
