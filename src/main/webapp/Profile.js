6d31ec88e710602a196de8683fcc1109873d0439
knutwii <knutwii@stud.ntnu.no>
2018-01-17 10:32:46 +0100

    updated profile information

--- src/main/webapp/Profile.js
+++ src/main/webapp/Profile.js
@@ -5,27 +5,88 @@ $(document).ready(function() {
 
         tableReadOnly.style.display = "none";
         tableEdit.style.display = "block";
+        sessionEmail();
     });
 
+    var sessionEmail = function () {
+        $.ajax({
+            type: 'GET',
+            url:'http://localhost:8080/scrum/rest/session',
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: function (data) {
+                var x;
+                x.email=data.email;
+                x.loggedOn=data.loggedOn;
+                console.log(sessionEmail());
+                return x;
+            }
+        })
+    };
 
     //Updating user information
     $('#Confirm').click(function () {
         var tableReadOnly = document.getElementById("tableUserInfoReadOnly");
         var tableEdit = document.getElementById("tableUserInfo");
 
-        tableReadOnly.style.display = "block";
-        tableEdit.style.display = "none";
-        console.log("confirmknapp trykket")
+        var nameField = $('#nameProfileField1').val();
+        var phoneField = $('#phoneField1').val();
+        if(nameField == ""){
+            alert("please fill out namefield");
+            $('#nameProfileField1').css({
+                "background-color": "yellow",
+            });
+        }else if(phoneField ==""){
+            alert("please fill out phonefield");
+            $('#phoneField1').css({
+                "background-color": "yellow",
+            });
+        }else {
 
+            $.ajax({
+                type: 'PUT',
+                url: 'http://localhost:8080/scrum/rest/user/abcqwe' /**+ sessionEmail()*/, //test
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                data: JSON.stringify({
+                    "name": nameField,
+                    "phone": phoneField,
+
+
+                }),
+                success: function (jqXHR, textStatus) {
+                    switch (jqXHR.status) {
+                        case 200:
+                            console.log("ajax update user info");
+                            alert("Information updated");
+                            break;
+                        case 404:
+                            alert("Could not update");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            });
+            tableReadOnly.style.display = "block";
+            tableEdit.style.display = "none";
+            console.log("confirmknapp trykket")
+        }
+    });
+
+    //Updating user password
+    $('#changePassword').click(function () {
         $.ajax({
-            type: 'POST',
-            url: 'http://localhost:8080/scrum/rest/user/abcqwe', //test
-            data: datafromToJSON(),
+            type: 'PUT',
+            url: 'http://localhost:8080/scrum/rest/user/tre@h.no' /**+ sessionEmail()*/, //test
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
-            complete: function (jqXHR, textStatus) {
+            data: JSON.stringify({
+                "password": $('#confirmPasswordField').val(),
+            }),
+            success: function (jqXHR, textStatus) {
                 switch (jqXHR.status) {
-                    case 200:
+                    case 204:
                         console.log("ajax update user info");
                         alert("Information updated");
                         break;
@@ -36,17 +97,18 @@ $(document).ready(function() {
                         break;
                 }
             }
-        })
+        });
+    });
 
-        function datafromToJSON() {
-            json = JSON.stringify({
-                "name": $('#nameProfileField1').val(),
-                "email": $('#phoneField1').val(),
-            });
-            console.log(json);
-            console.log("update user info");
-            return json;
-        }
+    /**(function datatoJSONupdateInfo() {
+        json = JSON.stringify({
+            "name": $('#nameProfileField1').val(),
+            "phone": $('#phoneField1').val(),
+            "email": 'abcqwe',
+            "password":'qwe'
 
-    })
+        });
+        console.log(json);
+        return json;
+    }*/
 });
