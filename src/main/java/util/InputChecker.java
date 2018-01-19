0c519296a6352acfea100dbb2249a32303187fca
bjornkpu <bk.punsvik@gmail.com>
2018-01-19 15:22:42 +0100

    Fixed removeExtraWhiteSpace in InputChecker

--- src/main/java/util/InputChecker.java
+++ src/main/java/util/InputChecker.java
@@ -193,7 +193,7 @@ public class InputChecker {
 	public static String removeExtraWhitespace(String s) {
 		if (s == null) return null;
 		s = s.trim();
-		s = s.replaceAll("[\\s&&[^ ]]+", " ");
+		s = s.replaceAll("[\\s&&[^ ]]+", "");
 		s = s.replaceAll(" +", " ");
 		return s;
 	}
