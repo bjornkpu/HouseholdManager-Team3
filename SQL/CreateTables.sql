3d5357ee93729ee7eb7ae509aea241660ddd06f6
Johan Martin Skavhaug <johanmsk@stud.ntnu.no>
2018-01-23 11:24:13 +0100

    Moved script for killing to top of createtables

--- SQL/CreateTables.sql
+++ SQL/CreateTables.sql
@@ -1,5 +1,10 @@
--- Kill all connections
-SELECT CONCAT('KILL ',id,';') AS run_this FROM information_schema.processlist WHERE user='root' AND info = 'SELECT * FROM processlist';
+/*Viser alle prosessene startet av brukeren vår
+SHOW PROCESSLIST ;
+
+Kopier resultatet av denne spørringa for å drepe alle connections som ikke utfører spørringer.
+SELECT CONCAT('KILL ',id,';') AS run_this FROM information_schema.processlist WHERE user='g_tdat2003_t3' AND info IS NULL ORDER BY id;
+ */
+
 
 -- Sletter tabeller
 SET FOREIGN_KEY_CHECKS = 0;
