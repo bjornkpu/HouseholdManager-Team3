c8b004ac077567e3efec055d6ed56775c46c0d8e
bjornkpu <bk.punsvik@gmail.com>
2018-01-24 10:30:30 +0100

    Done with rest in task

--- src/main/webapp/Tasks.js
+++ src/main/webapp/Tasks.js
@@ -1,70 +1,43 @@
 $(document).ready(function() {
-    var list;
-    var items;
-    var currentTaskMode = 1;
+    var groupId;
+    var tasks;
 
-    $(document).ready(function () {
-        var url='http://localhost:8080/scrum/rest/groups/'+getCookie("currenGroup")+'task/';
-        $.get(url, function(data, status){
-            list = data;
-
-            if(status === "success"){
-                console.log("Task content loaded successfully!");
-                renderTaskInformation(data);
-            }
-            if(status === "error"){
-                console.log("Error in loading ShoppingList content");
-            }
-        });
-    });
-
-//When clicking
-    $("#taskdropdown").on("click", "a.link", function(){
-        // currentTaskMode = lists[this.id].id;
-        renderTaskInformation(currentTaskMode);
-    });
-
-    function renderTaskInformation(data){
-        $("#tableTask").empty();
-
-        getTasks(data);
-
-        // $("#TaskName").text(lists[id].name);
-    }
-
-    function getTasks(id){
-        var url='http://localhost:8080/scrum/rest/groups/'+3+'/shoppingLists/'+id+'/items';
-
-        $.get(url, function(data, status){
-            if(status === "success"){
-                items = data;
-                console.log("Item content loaded successfully!");
-                setItemsInTable();
-            }
-            if(status === "error"){
-                console.log("Error in loading Item content");
-            }
-        });
-    }
-
-    function setItemsInTable(){
-        var len = items.length;
+    function setTasksInTable() {
+        var len = tasks.length;
         var table = document.getElementById("tableTask");
         while(table.rows.length > 0) {
             table.deleteRow(0);
         }
-
         for(var i = 0; i < len; i++){
-            var id = items[i].id;
+            var id = tasks[i].id;
             $("#tableTask").append(
-                "<tr>" +
+                "<tr id='row"+i+"'>" +
                 "<th scope=\"row\">"+(i+1)+"</th>" +
-                "<td>" + items[i].name + "</td>" +
-                "<td>" + items[i].status + "</td>" +
+                "<td>" + tasks[i].description + "</td>" +
+                "<td>" + tasks[i].assignedTo + "</td>" +
+                "<td>" + tasks[i].deadline + "</td>" +
                 "<td> <input value='"+ id +"' id='checkbox"+i+"' type='checkbox' ></td>" +
                 "</tr>"
             );
+            if(tasks[i].status===2){
+                $("#row"+i).addClass('boughtMarked');
+            }
         }
-        console.log("Added Items");
+        console.log("Added Tasks");
     }
+
+    function getTasksInGroup() {
+        console.log("Loading tasks from group " + groupId + "...");
+        $.get('http://localhost:8080/scrum/rest/groups/' + groupId + 'task/', function(data, status){
+            if(status === "success") {
+                tasks = data;
+                console.log("Tasks loaded successfully!");
+                setTasksInTable();
+            }
+            if(status === "error")      console.log("Error in loading tasks.");
+        });
+    }
+
+    groupId = getCookie("currentGroup");
+    getTasksInGroup();
 });
