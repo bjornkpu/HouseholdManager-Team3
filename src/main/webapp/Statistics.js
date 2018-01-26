95fcf5a84c537152825c5e8bacda2795b220a097
matseda <matseda@stud.ntnu.no>
2018-01-26 13:12:44 +0100

    Statistics

--- src/main/webapp/Statistics.js
+++ src/main/webapp/Statistics.js
@@ -3,23 +3,25 @@ var dataList;
 
  function makePie() {
      console.log("Test");
+     var pieList=[];
+     var totalValue=0;
+     for(var j=0;j<dataList.length;j++){
+         totalValue+=dataList[j].value;
+     }
+     for(var i=0;i<dataList.length;i++){
+         pieList.push({y: (dataList[i].value/totalValue)*100, label: dataList[i].key})
+     }
     var chart = new CanvasJS.Chart("chartContainer", {
         animationEnabled: true,
         title: {
-            text: "Desktop Search Engine Market Share - 2016"
+            text: "Completed tasks"
         },
         data: [{
             type: "pie",
             startAngle: 240,
             yValueFormatString: "##0.00\"%\"",
             indexLabel: "{label} {y}",
-            dataPoints: [
-                {y: 79.45, label: "Google"},
-                {y: 7.31, label: "Bing"},
-                {y: 7.06, label: "Baidu"},
-                {y: 4.91, label: "Yahoo"},
-                {y: 1.26, label: "Others"}
-            ]
+            dataPoints: pieList
         }]
     });
     chart.render();
